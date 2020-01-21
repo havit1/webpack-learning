8ac2cdf259b86770d0ab67762a18b76383a61176
Oleksandr <sashaka2013@gmail.com>
2020-01-21 19:29:15 +0200

    Initial commit

---
+++ src/babel.js
@@ -0,0 +1,18 @@
+async function start() {
+  return await Promise.resolve("async is working");
+}
+
+start().then(console.log);
+
+class Util {
+  static id = Date.now();
+}
+
+const unused = 42;
+console.log(unused);
+
+console.log("Util Id:", Util.id);
+
+import("lodash").then(() => {
+  console.log("Lodash", _.random(0, 42, true));
+});
